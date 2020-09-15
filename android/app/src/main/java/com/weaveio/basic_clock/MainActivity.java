79d90014d6742332a389570bf2e6d8af8b3c216f
asifimtiazahmed <a.imtiaz@gmail.com>
2020-09-14 23:55:50 -0400

    first commit

---
+++ android/app/src/main/java/com/weaveio/basic_clock/MainActivity.java
@@ -0,0 +1,6 @@
+package com.weaveio.basic_clock;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ name_log.dart
@@ -0,0 +1,165 @@
+//Git push for name:
+/*
+Sept 14th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 15th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 16th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 17th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 18th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 19th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 20th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 21th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 22th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 23th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 24th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 25th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 26th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+Sept 27th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 1st:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 5th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 8st:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 13st:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 14th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 15th:
+commit1:
+commit2:
+commit3:
+commit4:
+October 16th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+October 17th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+November 1st:
+commit1:
+commit2:
+commit3:
+commit4:
+
+November 2nd:
+commit1:
+commit2:
+commit3:
+commit4:
+
+November 3rd:
+commit1:
+commit2:
+commit3:
+commit4:
+
+November 4th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+November 7th:
+commit1:
+commit2:
+commit3:
+commit4:
+
+
+ */
